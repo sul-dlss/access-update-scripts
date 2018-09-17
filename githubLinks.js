515125822db60dcf2723d579d3461edfcf173681
Camille Villa <cvilla100@gmail.com>
2018-09-17 15:18:31 -0700

    Add JS script for grabbing GitHub PR links

---
+++ githubLinks.js
@@ -0,0 +1,33 @@
+// Paste this script in your browser console
+
+// Enter GitHub search params @ https://github.com/pulls
+// is:pr org:sul-dlss head:update-dependencies created:2018-09-17
+// example: https://github.com/pulls?utf8=%E2%9C%93&q=is%3Apr+org%3Asul-dlss+head%3Aupdate-dependencies+created%3A2018-09-17
+
+// Repo order used in autupdate.sh and status table
+// https://github.com/sul-dlss/access-update-scripts/blob/master/autupdate.sh
+var repoList= "purl stacks sul-embed purl-fetcher content_search course_reserves discovery-dispatcher earthworks exhibits library_hours_rails sul-bento-app sul-directory sul-requests sw-indexer SearchWorks revs dlme arclight-demo vatican_exhibits revs-indexer-service bassi_veratti editstore-updater mods_display_app mirador_sul frda relevancy_dashboard stanford-arclight searchworks-status"
+repoList = repoList.split(' ')
+
+// Retreive PR links
+var elements = Array.from(document.getElementsByClassName('js-navigation-open'))
+var links = []
+links = elements.filter(element => element.text.search("dependencies") > 0)
+links = links.map((link) => link.href + '/files')
+
+// Make a hash of repo name : PR link pairs
+var pairs = {}
+links.forEach((value) => {
+    pairs[value.split('/')[4]] = value
+})
+
+// Compose a copy n' paste-able table of repo names with either PR links OR empty cells
+var prettyTable = []
+repoList.forEach((repo) => {if (pairs[repo]){
+  prettyTable.push(`${repo}\t${pairs[repo]}`)
+} else {
+  prettyTable.push(`${repo}\t `)
+}})
+
+// join with new lines
+prettyTable.join('\n')
