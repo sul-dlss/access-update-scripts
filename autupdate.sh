5e9c396d0d0a087d229511ba83c18a4c5b47f815
Camille Villa <cvilla100@gmail.com>
2018-12-11 13:37:50 -0800

    Add harvestdor-indexer to portfolio

--- autupdate.sh
+++ autupdate.sh
@@ -13,7 +13,7 @@ GEM_SUCCESS_REPORTS_ARRAY=("/dev/null")
 NPM_SUCCESS_REPORTS_ARRAY=("/dev/null")
 
 # Ruby / Rails applications
-for i in purl stacks sul-embed purl-fetcher content_search course_reserves earthworks exhibits library_hours_rails sul-bento-app sul-directory sul-requests SearchWorks revs dlme arclight-demo vatican_exhibits revs-indexer-service bassi_veratti editstore-updater mods_display_app mirador_sul frda relevancy_dashboard stanford-arclight searchworks_traject_indexer; do
+for i in purl stacks sul-embed purl-fetcher content_search course_reserves earthworks exhibits library_hours_rails sul-bento-app sul-directory sul-requests SearchWorks revs dlme arclight-demo vatican_exhibits revs-indexer-service bassi_veratti editstore-updater mods_display_app mirador_sul frda relevancy_dashboard stanford-arclight searchworks_traject_indexer harvestdor-indexer; do
   echo $i
   cd $WORKSPACE/.autoupdate
   git clone git@github.com:sul-dlss/$i
--- githubLinks.js
+++ githubLinks.js
@@ -1,12 +1,12 @@
 // Paste this script in your browser console
 
 // Enter GitHub search params @ https://github.com/pulls
-// is:pr org:sul-dlss head:update-dependencies is:open 
+// is:pr org:sul-dlss head:update-dependencies is:open
 // example: https://github.com/pulls?utf8=✓&q=is%3Apr+org%3Asul-dlss+head%3Aupdate-dependencies+is%3Aopen+
 
 // Repo order used in autupdate.sh and status table
 // https://github.com/sul-dlss/access-update-scripts/blob/master/autupdate.sh
-var repoList= "purl stacks sul-embed purl-fetcher content_search course_reserves earthworks exhibits library_hours_rails sul-bento-app sul-directory sul-requests SearchWorks revs dlme arclight-demo vatican_exhibits revs-indexer-service bassi_veratti editstore-updater mods_display_app mirador_sul frda relevancy_dashboard stanford-arclight searchworks-status searchworks_traject_indexer"
+var repoList= "purl stacks sul-embed purl-fetcher content_search course_reserves earthworks exhibits library_hours_rails sul-bento-app sul-directory sul-requests SearchWorks revs dlme arclight-demo vatican_exhibits revs-indexer-service bassi_veratti editstore-updater mods_display_app mirador_sul frda relevancy_dashboard stanford-arclight searchworks-status searchworks_traject_indexer harvestdor-indexer"
 repoList = repoList.split(' ')
 
 // Retreive PR links
